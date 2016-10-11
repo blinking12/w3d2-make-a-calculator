311ed7036387b61edc6284d27c9c3f486771585d
Troy Denney <tvdenney@gmail.com>
2016-10-11 16:43:59 -0400

    fizzbuzz

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -1,24 +1,29 @@
 function FizzBuzz(n) {
+    var fizzbuzz = ''
+    for (var i = 1; i <= n; i++) {
 
+        // For every number that is a multiple of 3 and 5, return 'fizzbuzz'
+        if (i % 3 === 0 && i % 5 === 0) {
+            fizzbuzz += 'fizzbuzz'
+        }
+        // For every number that is a multiple of 3 (but not 5), return 'fizz'
+        else if (i % 3 === 0) {
+            fizzbuzz += 'fizz'
+        }
+        // For every number that is a multiple of 5 (but not 3), return 'buzz'
+        else if (i % 5 === 0) {
+            fizzbuzz += 'buzz'
+        }
+        // For every number that isn't a multiple of 3 or 5, return a period '.'
+        else {
+            fizzbuzz += '.'
+        }
+    }
+    return fizzbuzz
 }
-
+console.log(FizzBuzz(99))
 console.assert(FizzBuzz(1) === '.')
 console.assert(FizzBuzz(2) === '..')
 console.assert(FizzBuzz(3) === '..fizz')
 console.assert(FizzBuzz(5) === '..fizz.buzz')
 console.assert(FizzBuzz(10) === '..fizz.buzzfizz..fizzbuzz')
-console.log(FizzBuzz);
-
-/* original JavaScript Code used on COdecademy
-
-for (var i=1; i<=20; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log("FizzBuzz");
-    } else if (i % 3 === 0) {
-        console.log("Fizz");
-    } else if (i % 5 === 0) {
-        console.log("Buzz");
-    } else {
-        console.log(i);
-    }
-}
